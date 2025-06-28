e6d2b746a8ac6c4135e1541493cadd3fca9b9f64
Kirill Safronov <kiri2k3@gmail.com>
2025-06-28 22:27:57 +0300

    Расширение файла JSON

--- Server.js
+++ Server.js
@@ -3,14 +3,16 @@ const cors = require('cors'); // Добавьте эту строку
 const app = express();
 const PORT = 3000;
 
-app.use(cors()); // Разрешить все CORS-запросы
+app.use(cors());
 app.use(express.json());
 app.use(express.static('frontend'));
 
 app.get('/api/products', (req, res) => {
     res.json([
         { id: 1, name: "iPhone", price: 1200 },
-        { id: 2, name: "Samsung", price: 900 }
+        { id: 2, name: "Samsung", price: 900 },
+        { id: 3, name: "Футболка", price: 20 },
+        { id: 4, name: "Джинсы", price: 50 }
     ]);
 });
 
